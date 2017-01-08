dc48a8c641d44c4f080ae6d467b6314a7c3426fe
Ronen Nachmias <ronen.hamias@gmail.com>
2017-01-08 23:05:26 +0200

    check 127.0.0.1

--- src/test/java/io/scalecube/gateway/rabbitmq/RabbitPublisherTest.java
+++ src/test/java/io/scalecube/gateway/rabbitmq/RabbitPublisherTest.java
@@ -9,19 +9,19 @@ public class RabbitPublisherTest {
   @Test
   public void test_rabbit_publisher() {
     try {
-      RabbitPublisher publisher = new RabbitPublisher("localhost", 1, 1, null);
+      RabbitPublisher publisher = new RabbitPublisher("127.0.0.1", 1, 1, null);
     } catch (Exception e) {
       assertEquals(e.getMessage().toString(), "connect timed out");
     }
 
     try {
-      RabbitPublisher publisher = new RabbitPublisher("localhost", -1, 1, null);
+      RabbitPublisher publisher = new RabbitPublisher("127.0.0.1", -1, 1, null);
     } catch (Exception e) {
       assertEquals(e.getMessage().toString(), "connect timed out");
     }
 
     try {
-      RabbitPublisher publisher = new RabbitPublisher("localhost", -1, 1000, new BasicCredentials("a", "b"));
+      RabbitPublisher publisher = new RabbitPublisher("127.0.0.1", -1, 1000, new BasicCredentials("a", "b"));
     } catch (Exception e) {
       assertEquals(e.getMessage().toString(),
           "ACCESS_REFUSED - Login was refused using authentication mechanism PLAIN. For details see the broker logfile.");
